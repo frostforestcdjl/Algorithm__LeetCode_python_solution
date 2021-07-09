f18d38e5ad363a159ac1f51f2365e29f257eebda
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-07-10 03:43:44 +0800

    Update 0001. Two Sum.py

--- 0001. Two Sum.py
+++ 0001. Two Sum.py
@@ -1,3 +1,6 @@
+#Runtime: 4608 ms, faster than 10.55% of Python3 online submissions for Two Sum.
+#Memory Usage: 14.8 MB, less than 82.47% of Python3 online submissions for Two Sum.
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):

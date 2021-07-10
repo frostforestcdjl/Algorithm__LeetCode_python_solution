2ab7b311b51835c2f7faa3e6cbe51a7f3fb33e94
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-07-10 19:00:30 +0800

    Create 0232. Implement Queue using Stacks.py

---
+++ 0232. Implement Queue using Stacks.py
@@ -0,0 +1,44 @@
+#Runtime: 20 ms, faster than 99.06% of Python3 online submissions for Implement Queue using Stacks.
+#Memory Usage: 14.4 MB, less than 43.00% of Python3 online submissions for Implement Queue using Stacks.
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.queue.pop(0)
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if len(self.queue) == 0:
+            return True
+        return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()

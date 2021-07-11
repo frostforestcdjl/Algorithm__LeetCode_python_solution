b573ed6532f5e2dee56db58481c902d0fca95b65
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-07-11 23:02:33 +0800

    Create 0344. Reverse String.py

---
+++ 0344. Reverse String.py
@@ -0,0 +1,9 @@
+#Runtime: 188 ms, faster than 93.85% of Python3 online submissions for Reverse String.
+#Memory Usage: 18.5 MB, less than 84.34% of Python3 online submissions for Reverse String.
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()

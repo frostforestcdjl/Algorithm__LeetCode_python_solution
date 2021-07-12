837542185740063a200448e283631fd7bca723b3
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-07-13 03:29:26 +0800

    Update 0392. Is Subsequence.py

--- 0392. Is Subsequence.py
+++ 0392. Is Subsequence.py
@@ -1,13 +1,13 @@
-#Runtime: 32 ms, faster than 70.88% of Python3 online submissions for Is Subsequence.
+#Runtime: 24 ms, faster than 97.03% of Python3 online submissions for Is Subsequence.
 #Memory Usage: 14.2 MB, less than 74.59% of Python3 online submissions for Is Subsequence.
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if len(s) == 0:
-            return True
         for i in t:
-            if i == s[0]:
-                    s = s[1:]
             if len(s) == 0:
                 return True
-        return False 
+            if i == s[0]:
+                    s = s[1:]
+        if len(s) == 0:
+                return True
+        return False

be98a117c36e291309c6ad04f5e65370c10f50f4
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-07-13 17:40:42 +0800

    Update 0405. Convert a Number to Hexadecimal.py

--- 0405. Convert a Number to Hexadecimal.py
+++ 0405. Convert a Number to Hexadecimal.py
@@ -1,4 +1,4 @@
-#Runtime: 28 ms, faster than 81.62% of Python3 online submissions for Convert a Number to Hexadecimal.
+#Runtime: 24 ms, faster than 94.55% of Python3 online submissions for Convert a Number to Hexadecimal.
 #Memory Usage: 14.3 MB, less than 44.61% of Python3 online submissions for Convert a Number to Hexadecimal.
 
 class Solution:

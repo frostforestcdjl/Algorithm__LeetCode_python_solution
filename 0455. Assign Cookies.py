ae321c63e90a6e65d18d81466ff5f2107b44ee2b
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-07-14 15:30:26 +0800

    Update 0455. Assign Cookies.py

--- 0455. Assign Cookies.py
+++ 0455. Assign Cookies.py
@@ -1,14 +1,14 @@
-#Runtime: 3232 ms, faster than 5.06% of Python3 online submissions for Assign Cookies.
-#Memory Usage: 15.8 MB, less than 82.95% of Python3 online submissions for Assign Cookies.
+#Runtime: 148 ms, faster than 97.44% of Python3 online submissions for Assign Cookies.
+#Memory Usage: 15.8 MB, less than 90.50% of Python3 online submissions for Assign Cookies.
 
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         c_n = 0
         g.sort(reverse=True)
+        s.sort()
         for i in range(len(g)):
-            if len(s) == 0:
-                break
-            if g[i] <= max(s):
+            if len(s) == 0: return c_n
+            if g[i] <= s[-1]:
                 c_n += 1
-                s.remove(max(s))
+                s.pop(-1)
         return c_n

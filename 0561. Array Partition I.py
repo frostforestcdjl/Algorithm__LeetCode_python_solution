191262ddbd2c8d88ce3b100b1668bbd9d7ab9d56
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-07-18 18:53:15 +0800

    Update 0561. Array Partition I.py

--- 0561. Array Partition I.py
+++ 0561. Array Partition I.py
@@ -1,10 +1,7 @@
-#Runtime: 284 ms, faster than 35.64% of Python3 online submissions for Array Partition I.
-#Memory Usage: 16.5 MB, less than 98.28% of Python3 online submissions for Array Partition I.
+#Runtime: 252 ms, faster than 94.47% of Python3 online submissions for Array Partition I.
+#Memory Usage: 16.7 MB, less than 90.16% of Python3 online submissions for Array Partition I.
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
-        min_sum = 0
-        for i in range(len(nums)//2):
-            min_sum += min(nums[2*i], nums[2*i+1])
-        return min_sum
+        return sum(nums[::2])

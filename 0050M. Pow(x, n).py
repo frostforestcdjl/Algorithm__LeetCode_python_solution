ef5bdbbfb988a7977a7dc8ff43fe2c2e61a47784
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-01 17:02:54 +0800

    Create 0050M. Pow(x, n).py

---
+++ 0050M. Pow(x, n).py
@@ -0,0 +1,6 @@
+#Runtime: 24 ms, faster than 95.96% of Python3 online submissions for Pow(x, n).
+#Memory Usage: 14.2 MB, less than 79.13% of Python3 online submissions for Pow(x, n).
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x, n)

f7d551e69b81983077e4e7b43a32a93151754d24
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-03 17:34:43 +0800

    Create 0064M. Minimum Path Sum.py

---
+++ 0064M. Minimum Path Sum.py
@@ -0,0 +1,13 @@
+#Runtime: 84 ms, faster than 99.53% of Python3 online submissions for Minimum Path Sum.
+#Memory Usage: 15.6 MB, less than 63.66% of Python3 online submissions for Minimum Path Sum.
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        for i in range(1, len(grid)):
+            grid[i][0] += grid[i-1][0]
+        for i in range(1, len(grid[0])):
+            grid[0][i] += grid[0][i-1]
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        return grid[-1][-1]

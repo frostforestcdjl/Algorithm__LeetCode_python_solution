5a29ae98c6a072947606a65eb398009b8c42bd21
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-03 23:39:50 +0800

    Create 0709. To Lower Case.py

---
+++ 0709. To Lower Case.py
@@ -0,0 +1,6 @@
+#Runtime: 24 ms, faster than 95.33% of Python3 online submissions for To Lower Case.
+#Memory Usage: 14.1 MB, less than 88.38% of Python3 online submissions for To Lower Case.
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()

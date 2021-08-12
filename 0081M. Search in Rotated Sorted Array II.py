1c83ed400cab0c35f67e1b968a347f514eca76d4
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-12 19:22:25 +0800

    Create 0081M. Search in Rotated Sorted Array II.py

---
+++ 0081M. Search in Rotated Sorted Array II.py
@@ -0,0 +1,6 @@
+#Runtime: 48 ms, faster than 90.68% of Python3 online submissions for Search in Rotated Sorted Array II.
+#Memory Usage: 15 MB, less than 63.75% of Python3 online submissions for Search in Rotated Sorted Array II.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        return target in nums

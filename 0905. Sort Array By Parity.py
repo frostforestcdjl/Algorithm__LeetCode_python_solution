24d3c514f9c31edb24c00fe09f0398c945f62226
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-19 00:26:09 +0800

    Create 0905. Sort Array By Parity.py

---
+++ 0905. Sort Array By Parity.py
@@ -0,0 +1,29 @@
+#Runtime: 64 ms, faster than 99.79% of Python3 online submissions for Sort Array By Parity.
+#Memory Usage: 15.1 MB, less than 57.98% of Python3 online submissions for Sort Array By Parity.
+  
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        odd = []
+        even = []
+        for i in range(len(nums)):
+            if nums[i]%2 == 1:
+                odd.append(nums[i])
+            else:
+                even.append(nums[i])
+        return even + odd
+      
+
+#Runtime: 1316 ms, faster than 5.01% of Python3 online submissions for Sort Array By Parity.
+#Memory Usage: 14.9 MB, less than 94.70% of Python3 online submissions for Sort Array By Parity.
+
+class Solution2:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            if nums[i]%2 == 1:
+                for j in range(len(nums)-i):
+                    if nums[~j]%2 == 0:
+                        nums[i], nums[~j] = nums[~j], nums[i]
+                        break
+                    if i - ~j == len(nums):
+                        return nums
+        return nums

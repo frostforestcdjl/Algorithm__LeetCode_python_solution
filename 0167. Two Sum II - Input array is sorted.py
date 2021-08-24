710d02d1927e475e4c9b8de4de3c978f6dc40f9b
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-24 18:12:18 +0800

    Update 0167. Two Sum II - Input array is sorted.py

--- 0167. Two Sum II - Input array is sorted.py
+++ 0167. Two Sum II - Input array is sorted.py
@@ -1,4 +1,19 @@
+#Runtime: 60 ms, faster than 83.87% of Python3 online submissions for Two Sum II - Input array is sorted.
+#Memory Usage: 14.6 MB, less than 86.78% of Python3 online submissions for Two Sum II - Input array is sorted.
+    
 class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in set(numbers):
+            ind = numbers.index(i)
+            test_l = numbers[:ind] + numbers[ind+1:]
+            if target - i in test_l:
+                r2 = numbers.index(target - i)
+                if numbers.index(target - i) == ind:
+                    return ind + 1, ind + 2
+                return min(ind, r2) + 1, max(ind, r2) + 1
+            
+
+class Solution2:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for i in range(len(numbers)):
             if numbers.count(numbers[i]) > 2:

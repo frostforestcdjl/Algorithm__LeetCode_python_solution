5d550d32a69b9ff5252be0d5a1df89a767b55485
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-28 04:38:38 +0800

    Update 0003M. Longest Substring Without Repeating Characters.py

--- 0003M. Longest Substring Without Repeating Characters.py
+++ 0003M. Longest Substring Without Repeating Characters.py
@@ -1,4 +1,28 @@
+#Runtime: 279 ms, faster than 19.42% of Python3 online submissions for Longest Substring Without Repeating Characters.
+#Memory Usage: 14.1 MB, less than 99.12% of Python3 online submissions for Longest Substring Without Repeating Characters.
+
 class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        if len(s) == len(set(s)):
+            return len(s)
+        l = 0
+        r = 1
+        max_n = 1
+        while r < len(s):
+            if len(set(s[l:r])) == r - l:
+                max_n = max(max_n, r -l)
+                r += 1
+            else:
+                l += 1
+                max_n = max(max_n, r-l)
+        if s[r-1] not in s[l:r-1]:
+            return max(max_n, r-l)
+        return max_n
+    
+
+class Solution2:
     def lengthOfLongestSubstring(self, s: str) -> int:
         length = [0]
         for i in range(len(s)):

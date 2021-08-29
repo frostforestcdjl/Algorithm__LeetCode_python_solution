ba93e48531c8751e71690dfec3d2e7641ba9e4c3
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-29 18:55:13 +0800

    Update 0065H. Valid Number.py

--- 0065H. Valid Number.py
+++ 0065H. Valid Number.py
@@ -7,60 +7,47 @@ class Solution:
         if s[0] == 'e' or s[-1] == 'e' or s[-1] == '+' or s[-1] == '-':
             return False
         text = list(s)
-        print(text)
         ava_l = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', 'e', 'E'}
         num_l = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
         n_check = False
         for i in text:
             if i not in ava_l:
-                print('check1', i)
                 return False
             if i in num_l:
                 n_check = True
         if n_check == False:
             return False
         if s.count('e') > 1 or s.count('.') > 1:
-            print('check2')
             return False
         if 'e' in s and '.' in s:
             if s.index('.') > s.index('e') or (s.index('.') + 1 == s.index('e') and s.index('.') == 0):
-                print('check3')
                 return False
         if s.count('+') + s.count('-') > 2:
-            print('check4')
             return False
         if s.count('+') == 1:
             if s.count('e') == 1:
                 if s.index('+') != 0 and s.index('+') != s.index('e') + 1 or s.index('+') == s.index('e') - 1:
-                    print('check5')
                     return False
             else:
                 if s.index('+') != 0:
-                    print('check6')
                     return False
         elif s.count('+') == 2:
             if s.count('e') != 1:
-                print('check7')
                 return False
             else:
                 if s.index('+') != 0 or s[s.index('e')+1] != '+':
-                    print('check8')
                     return False
         if s.count('-') == 1:
             if s.count('e') == 1:
                 if s.index('-') != 0 and s.index('-') != s.index('e') + 1 or s.index('-') == s.index('e') - 1:
-                    print('check9')
                     return False
             else:
                 if s.index('-') != 0:
-                    print('check10')
                     return False
         elif s.count('-') == 2:
             if s.count('e') != 1:
-                print('check11')
                 return False
             else:
                 if s.index('-') != 0 and s[s.index('e')+1] != '-':
-                    print('check12')
                     return False
         return True

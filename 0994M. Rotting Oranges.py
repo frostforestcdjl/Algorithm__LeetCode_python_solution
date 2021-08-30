90d1fe9ba6ad998f49c5842744b5476f9a25a21b
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-08-30 23:53:38 +0800

    Create 0994M. Rotting Oranges.py

---
+++ 0994M. Rotting Oranges.py
@@ -0,0 +1,39 @@
+#Runtime: 67 ms, faster than 16.85% of Python3 online submissions for Rotting Oranges.
+#Memory Usage: 13.9 MB, less than 99.66% of Python3 online submissions for Rotting Oranges.
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rot = []
+        fresh = []
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2:
+                    rot.append([i,j])
+                elif grid[i][j] == 1:
+                    fresh.append([i,j])
+        count = 0
+        while rot and fresh:
+            new_rot = []
+            for cand in rot:
+                i, j = cand[0], cand[1]
+                if [i-1,j] in fresh:
+                    grid[i-1][j] = 2
+                    fresh.remove([i-1,j])
+                    new_rot.append([i-1,j])
+                if [i+1,j] in fresh:
+                    grid[i+1][j] = 2
+                    fresh.remove([i+1,j])
+                    new_rot.append([i+1,j])
+                if [i,j-1] in fresh:
+                    grid[i][j-1] = 2
+                    fresh.remove([i,j-1])
+                    new_rot.append([i,j-1])
+                if [i,j+1] in fresh:
+                    grid[i][j+1] = 2
+                    fresh.remove([i,j+1])
+                    new_rot.append([i,j+1])
+            rot = new_rot
+            count += 1
+        if fresh:
+            return -1
+        return count

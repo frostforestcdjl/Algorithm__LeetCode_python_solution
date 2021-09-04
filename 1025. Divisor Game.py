a147a280b3e29a33448fd970f2886db5881b508e
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-09-05 02:09:40 +0800

    Create 1025. Divisor Game.py

---
+++ 1025. Divisor Game.py
@@ -0,0 +1,6 @@
+#Runtime: 24 ms, faster than 95.26% of Python3 online submissions for Divisor Game.
+#Memory Usage: 14.1 MB, less than 89.12% of Python3 online submissions for Divisor Game.
+
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n%2 == 0

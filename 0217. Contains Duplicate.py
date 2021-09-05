08f152a190076c458497aa7b08b8f79b6e6c61e4
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-09-05 16:58:13 +0800

    Update 0217. Contains Duplicate.py

--- 0217. Contains Duplicate.py
+++ 0217. Contains Duplicate.py
@@ -1,7 +1,15 @@
+#Runtime: 108 ms, faster than 97.66% of Python3 online submissions for Contains Duplicate.
+#Memory Usage: 19.9 MB, less than 79.38% of Python3 online submissions for Contains Duplicate.
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+
 #Runtime: 124 ms, faster than 45.46% of Python3 online submissions for Contains Duplicate.
 #Memory Usage: 18.6 MB, less than 98.45% of Python3 online submissions for Contains Duplicate.
 
-class Solution:
+class Solution2:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
         for i in range(len(nums)):

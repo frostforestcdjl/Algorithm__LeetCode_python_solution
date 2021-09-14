8a81cb06e5d014b8a0ba273e2ea764978f131189
frostforest <49707220+frostforestcdjl@users.noreply.github.com>
2021-09-14 19:14:14 +0800

    Update 0020. Valid Parentheses.py

--- 0020. Valid Parentheses.py
+++ 0020. Valid Parentheses.py
@@ -1,4 +1,28 @@
+#Runtime: 20 ms, faster than 99.22% of Python3 online submissions for Valid Parentheses.
+#Memory Usage: 14.3 MB, less than 34.39% of Python3 online submissions for Valid Parentheses.
+
 class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if stack:
+                if i == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif i == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif i == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+        return not stack
+
+
+#Runtime: 2205 ms, faster than 5.48% of Python3 online submissions for Valid Parentheses.
+#Memory Usage: 14.2 MB, less than 63.51% of Python3 online submissions for Valid Parentheses.
+    
+class Solution2:
     def isValid(self, s: str) -> bool:
         x = list(s)
         y = 0
